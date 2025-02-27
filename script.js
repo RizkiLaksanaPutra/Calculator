22fad8d25592a3bd27174b043f5e53666b495076
Rizki Laksana Putra <120088587+RizkiLaksanaPutra@users.noreply.github.com>
2025-02-27 20:45:51 +0700

    Refactor button elements to use data attributes for operators and numbers

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-const zero = document.getElementById("zero");
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const plus = document.getElementById("plus")
-
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
 const currentNumber = document.getElementById("current-number");
-
-const numberButtons = [
-  zero,
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight,
-  nine,
-];
 
 let firstNumber = null;
 let operator = null;
 
-numberButtons.forEach((button, index) => {
+[...numberButtons].reverse().forEach((button, index) => {
   button.addEventListener("click", () => {
     currentNumber.textContent += index;
     firstNumber = Number(currentNumber.textContent);
